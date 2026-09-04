8ed66e51aeb829224e031aab6a7fbf4e4a0dd4c6
Juan Camilo Mora <camilo2652@gmaill.com>
2026-09-03 22:54:37 -0500

    StackUsingArray

---
+++ Backend/StackUsingArray.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Backend;
+
+public class StackUsingArray<T>
+{
+    private T[] _stack;
+    private int _top;
+
+    public StackUsingArray(int capacity)
+    {
+        _stack = new T[capacity];
+        _top = -1;
+    }
+
+    public bool IsFull { get => _top == _stack.Length - 1; }
+
+    public bool IsEmpty { get => _top == -1; }
+
+    public void Push(T item)
+    {
+        if (IsFull)
+        {
+            throw new InvalidOperationException("Stack is full");
+        }
+        _stack[++_top] = item;
+    }
+    public T Pop()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        return _stack[_top--];
+    }
+    public T Peak()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        return _stack[_top];
+    }
+}
---
+++ Frontend/Program.cs
@@ -0,0 +1,45 @@
+﻿using Backend;
+
+var stack = new StackUsingArray<string>(10);
+
+    var option = string.Empty;
+    do
+    {
+        option = Menu();
+        try
+        {
+            switch (option)
+            {
+                case "1":
+                    Console.Write("Digite elemento: ");
+                    stack.Push(Console.ReadLine()!);
+                    break;
+                case "2":
+                    Console.WriteLine($"Elemento desapilado: {stack.Pop()} ");
+                    break;
+                case "3":
+                    Console.WriteLine($"Elemento en tope de pila: {stack.Peak()} ");
+                    break;
+                default:
+                    Console.WriteLine("Opción no valida: ");
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex.Message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    } while (option != "0");
+
+
+    string Menu()
+    {
+        Console.WriteLine("1. Apilar.");
+        Console.WriteLine("2. Desapilar.");
+        Console.WriteLine("3. Ver tope de la pila.");
+        Console.WriteLine("0. Salir");
+        Console.Write("Digite su opción: ");
+        return Console.ReadLine()!;
+    }
